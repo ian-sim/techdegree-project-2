17b10dc1609950972be4ad23b271c66f57bd9bbc
ian-sim <i.simpson8891@gmail.com>
2020-04-27 11:26:43 +0800

    Refactored code
    Refactored js file to improve readability

--- list_pagination_and_filtering-v1/js/script.js
+++ list_pagination_and_filtering-v1/js/script.js
@@ -1,43 +1,3 @@
-/******************************************
-Treehouse Techdegree:
-FSJS project 2 - List Filter and Pagination
-******************************************/
-
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
-
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
-
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
-
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
-
 // Create global variables representing the list of students and the max number of results per page
 const studentList = document.querySelectorAll("li");
 const resultsPerPage = 10;
@@ -57,29 +17,36 @@ function showPage(list, page) {
 		}
 	}
 }
+// create function to create, append and assign property values to an element
+function createAssignAppend(
+	elementName,
+	appendTo,
+	property1 = "",
+	value1 = "",
+	property2 = "",
+	value2 = ""
+) {
+	const element = document.createElement(elementName);
+	element[property1] = value1;
+	element[property2] = value2;
+	appendTo.appendChild(element);
+	return element;
+}
 // Create function that calculates the number of page links needed, creates the links and appends them to the page
 function appendPageLinks(list) {
 	const pageDiv = document.querySelector(".page");
-	const linkDiv = document.createElement("div");
-	linkDiv.className = "pagination";
-	pageDiv.appendChild(linkDiv);
-	const linkUl = document.createElement("ul");
-	linkDiv.appendChild(linkUl);
+	const linkDiv = createAssignAppend("div", pageDiv, "className", "pagination");
+	const linkUl = createAssignAppend("ul", linkDiv);
 	// Create variable that calculates the number of page links needed and rounds up
 	const numberOfLinks = Math.ceil(list.length / resultsPerPage);
 	// Create page links, set properties and append to page
 	for (let i = 1; i <= numberOfLinks; i++) {
-		const li = document.createElement("li");
-		const a = document.createElement("a");
-		a.href = "#";
-		a.textContent = i;
-		li.appendChild(a);
-		linkUl.appendChild(li);
+		const li = createAssignAppend("li", linkUl);
+		const a = createAssignAppend("a", li, "href", "#", "textContent", i);
 	}
 	// Select all page links and initialize first link class to 'active'
 	const pageLinks = document.querySelectorAll('a[href="#"]');
 	pageLinks[0].className = "active";
-
 	// Add event listeners to each page link
 	for (let j = 0; j < pageLinks.length; j++) {
 		pageLinks[j].addEventListener("click", (e) => {
