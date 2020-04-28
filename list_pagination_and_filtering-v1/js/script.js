d2a397c1652d395dedd600228918c5751e41599f
ian-sim <i.simpson8891@gmail.com>
2020-04-28 17:23:47 +0800

    Added search functionality
    Added code for search functionality for 'exceeds expectations' grade.
    Updated comments to make descriptions more clear

--- list_pagination_and_filtering-v1/js/script.js
+++ list_pagination_and_filtering-v1/js/script.js
@@ -4,7 +4,7 @@ const resultsPerPage = 10;
 
 // Create function that determines and displays the selected page of results
 function showPage(list, page) {
-	// Calculate the start and end of the results to be displayed based on arguments
+	// Calculate the start and end indices of the results to be displayed based on arguments
 	let startIndex = page * resultsPerPage - resultsPerPage;
 	let endIndex = page * resultsPerPage;
 
@@ -17,7 +17,26 @@ function showPage(list, page) {
 		}
 	}
 }
-// create function to create, append and assign property values to an element
+
+// Create apology for zero results message (FOR 'EXCEEDS' GRADE REQUIREMENTS)
+const pageDiv = document.querySelector("div.page");
+const listUl = document.querySelector("ul.student-list");
+const apologyDiv = document.createElement("div");
+apologyDiv.className = "js-apology";
+pageDiv.insertBefore(apologyDiv, listUl);
+const noResultsMessage = createAssignAppend(
+	"h2",
+	apologyDiv,
+	"textContent",
+	"Sorry, there were no results from your search.",
+	"textAlign",
+	"center"
+);
+noResultsMessage.style.fontSize = "1.4rem";
+//Initialize no results message to hidden
+noResultsMessage.style.display = "none";
+
+// Create function to create, append and assign property values to an element
 function createAssignAppend(
 	elementName,
 	appendTo,
@@ -34,20 +53,25 @@ function createAssignAppend(
 }
 // Create function that calculates the number of page links needed, creates the links and appends them to the page
 function appendPageLinks(list) {
+	if (!list.length) {
+		return (noResultsMessage.style.display = "");
+	} else {
+		noResultsMessage.style.display = "none";
+	}
 	const pageDiv = document.querySelector(".page");
 	const linkDiv = createAssignAppend("div", pageDiv, "className", "pagination");
 	const linkUl = createAssignAppend("ul", linkDiv);
-	// Create variable that calculates the number of page links needed and rounds up
+	// Create variable that calculates the number of page links needed and rounds up to next integer
 	const numberOfLinks = Math.ceil(list.length / resultsPerPage);
 	// Create page links, set properties and append to page
 	for (let i = 1; i <= numberOfLinks; i++) {
 		const li = createAssignAppend("li", linkUl);
 		const a = createAssignAppend("a", li, "href", "#", "textContent", i);
 	}
-	// Select all page links and initialize first link class to 'active'
+	// Select all page links and initialize class names to ''. Set page 1 link class to 'active'
 	const pageLinks = document.querySelectorAll('a[href="#"]');
 	pageLinks[0].className = "active";
-	// Add event listeners to each page link
+	// Add event listeners to each page link to change class names based on event.target
 	for (let j = 0; j < pageLinks.length; j++) {
 		pageLinks[j].addEventListener("click", (e) => {
 			const pageNumber = parseInt(e.target.textContent);
@@ -55,10 +79,81 @@ function appendPageLinks(list) {
 				pageLinks[k].className = "";
 			}
 			e.target.className = "active";
-			showPage(studentList, pageNumber);
+			showPage(list, pageNumber);
 		});
 	}
+	showPage(list, 1);
 }
 // Call showPage for initial page on screen then call appendPageLinks to add the page links
 showPage(studentList, 1);
 appendPageLinks(studentList);
+
+// THE CODE BEYOND THIS POINT IS FOR THE 'EXCEEDS' GRADE REQUIREMENTS!
+
+// Create input field and search button for search functionality
+const headerDiv = document.querySelector(".page-header");
+const searchDiv = createAssignAppend(
+	"div",
+	headerDiv,
+	"className",
+	"student-search"
+);
+const searchInput = createAssignAppend(
+	"input",
+	searchDiv,
+	"placeholder",
+	"Search for students..."
+);
+const searchButton = createAssignAppend(
+	"button",
+	searchDiv,
+	"textContent",
+	"Search"
+);
+
+// Create function to remove previous page links
+function removeLinks() {
+	const prevLinks = document.querySelector(".pagination");
+	if (prevLinks) {
+		const parent = prevLinks.parentNode;
+		parent.removeChild(prevLinks);
+	}
+}
+
+// Create function to compare user input to student names and display any results found with a match
+function searchName(searchInput, namesList) {
+	removeLinks();
+	const foundList = [];
+	for (let i = 0; i < namesList.length; i++) {
+		namesList[i].style.display = "none";
+		const name = namesList[
+			i
+		].firstElementChild.children[1].textContent.toLowerCase();
+		const input = searchInput.value.toLowerCase();
+		if (input.length !== 0 && name.includes(input)) {
+			namesList[i].style.display = "";
+			foundList.push(namesList[i]);
+		}
+	}
+	return foundList;
+}
+
+// Event listener for search Button to search and display results, if any
+searchButton.addEventListener("click", () => {
+	if (searchInput.value) {
+		appendPageLinks(searchName(searchInput, studentList));
+	} else {
+		removeLinks();
+		appendPageLinks(studentList);
+	}
+});
+
+// Event listener for keyup in input field to search and display results, if any
+searchInput.addEventListener("keyup", () => {
+	if (searchInput.value) {
+		appendPageLinks(searchName(searchInput, studentList));
+	} else {
+		removeLinks();
+		appendPageLinks(studentList);
+	}
+});
